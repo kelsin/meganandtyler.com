3a2baa3e56b2d76cc62331309af53d90df7d4d7b
Christopher Giroir <kelsin@valefor.com>
2015-07-27 21:23:44 -0700

    Adding ability to build and preview from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
 var gulp = require('gulp');
 var pages = require('gulp-gh-pages');
+var wintersmith = require('run-wintersmith');
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['build'], function() {
     return gulp.src('./build/**/*')
     .pipe(pages());
+});
+
+gulp.task('build', function(done) {
+    wintersmith.build(done);
+});
+
+gulp.task('default', function() {
+    wintersmith.preview();
 });
