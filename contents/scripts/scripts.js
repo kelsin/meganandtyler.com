87eaaf14b48874664a9a8e2e514c22b30373b325
Christopher Giroir <kelsin@valefor.com>
2015-07-27 23:10:05 -0700

    About, Party and RSVP content

--- contents/scripts/scripts.js
+++ contents/scripts/scripts.js
@@ -3,12 +3,11 @@
   angular.module('MeganAndTyler').config(function($routeProvider, $locationProvider){
     $locationProvider.html5Mode(false);
     $routeProvider
-      .when('/about', {
-        templateUrl: 'about.html'
-      })
-      .otherwise({
-        templateUrl: 'home.html'
-      });
+      .when('/about', { templateUrl: 'about.html' })
+      .when('/party', { templateUrl: 'party.html' })
+      .when('/rsvp', { templateUrl: 'rsvp.html' })
+      .when('/', { templateUrl: 'home.html' })
+      .otherwise({ redirectTo: '/' });
   });
 
 })();
